b05ffd3ea59cd6e2ed7154ed3847c4c59af9925c
choiJieun <eaee20171@gmail.com>
2025-11-14 10:03:59 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -2,47 +2,27 @@ n, m = map(int, input().split())
 
 board = [input() for _ in range(n)]
 
-def checkboard(board:list):
-    c1, c2 = 0, 0
+def checkBW(s1:str, s2:str, board:list):
+    change = 0
     
-    #start: black
-    # BWBW...
+    # s1 s2 s1 s2...
     for i in range(0, len(board), 2):
         for j in range(0, len(board[i]), 2):
-            if board[i][j] != 'B': c1 += 1
+            if board[i][j] != s1: change += 1
         for k in range(1, len(board[i]), 2):
-            if board[i][k] != 'W': c1 += 1
-
-    # WBWB... 판별
-    for i in range(1, len(board), 2):
-        for j in range(0, len(board[i]), 2):
-            if board[i][j] != 'W': c1 += 1
-        for k in range(1, len(board[i]), 2):
-            if board[i][k] != 'B': c1 += 1
-            
-
-    #start : white
+            if board[i][k] != s2: change += 1
     
-    # WBWB... 판별
-    for i in range(0, len(board), 2):
-        for j in range(0, len(board[i]), 2):
-            if board[i][j] != 'W': c2 += 1
-        for k in range(1, len(board[i]), 2):
-            if board[i][k] != 'B': c2 += 1
-
-    # BWBW...
+    #s2 s1 s2 s1 ...
     for i in range(1, len(board), 2):
         for j in range(0, len(board[i]), 2):
-            if board[i][j] != 'B': c2 += 1
+            if board[i][j] != s2: change += 1
         for k in range(1, len(board[i]), 2):
-            if board[i][k] != 'W': c2 += 1
+            if board[i][k] != s1: change += 1
             
-    return min(c1, c2)
+    return change
 
-minNum = []
-for i in range(n - 7):
-    for j in range(m - 7):
-        temp = [row[j:8+j] for row in board[i:8+i]]
-        minNum.append(checkboard(temp))
+minNum = [min(checkBW('B', 'W',[row[j:8+j] for row in board[i:8+i]]), 
+              checkBW('W', 'B', [row[j:8+j] for row in board[i:8+i]]))
+          for i in range(n - 7) for j in range(m - 7)]
 
 print(min(minNum))
