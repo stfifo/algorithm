8e1a4550f3076d468e2db79467725a6c9c35ba8d
choiJieun <eaee20171@gmail.com>
2026-01-14 16:53:45 +0900

    [Silver IV] Title: 30, Time: 76 ms, Memory: 36560 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.py
+++ 백준/Silver/10610. 30/30.py
@@ -2,9 +2,6 @@ n = input()
 n_lst = list(map(int, list(n)))
 n_lst.sort(reverse=True)
 if n_lst[-1] == 0 and sum(n_lst)%3 == 0:
-    result = ''
-    for num in n_lst:
-        result += str(num)
-    print(result)
+    print(*n_lst, sep='')
 else:
     print(-1)
