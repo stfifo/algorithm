055ab1a69077e56e90eb37058130ad021e321e06
choiJieun <eaee20171@gmail.com>
2026-01-29 19:09:27 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -7,21 +7,20 @@ int main() {
     cin.tie(NULL);
     vector<int> height(9);
     int i, j;
-    for (i=0; i<9; ++i) {
-        cin >> height[i];
-    }
+    for (i=0; i<9; ++i) cin >> height[i];
+
     int x = accumulate(height.begin(), height.end(), 0) - 100;
 
     for (i=0; i<9; ++i) {
         for (j=0; j < i; ++j) {
             if (height[i]+height[j] == x) {
-                height.erase(height.begin()+i);
-                height.erase(height.begin()+j);
+                height[i] = 0;
+                height[j] = 0;
+
                 sort(height.begin(), height.end());
 
-                for (i=0; i<7; ++i) {
-                    cout << height[i] << '\n';
-                }
+                for (i=2; i<9; ++i) cout << height[i] << '\n';
+                return 0;
             }
         }
     }
