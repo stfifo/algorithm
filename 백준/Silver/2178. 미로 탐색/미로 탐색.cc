365039123018439c46b7d7a19ee3870b08979f03
choiJieun <eaee20171@gmail.com>
2026-02-10 16:53:03 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2200 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define x first
+#define y second
+
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+int n, m;
+int dist[102][102];
+string board[102];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> m;
+
+    for (int i=0; i<n; ++i)
+        cin >> board[i];
+
+    for (int i=0; i<n; ++i) fill(dist[i], dist[i]+m, -1);
+
+    queue<pair<int, int>> Q;
+    dist[0][0] = 0;
+    Q.push({0, 0});
+
+    while (!Q.empty()) {
+
+        pair<int, int> cur = Q.front();
+        Q.pop();
+
+        for (int dir=0; dir<4; ++dir) {
+            int nx = cur.x + dx[dir];
+            int ny = cur.y + dy[dir];
+
+            if (nx <0 || nx >= n || ny < 0 || ny >= m) continue;
+            if (dist[nx][ny]>=0 || board[nx][ny] != '1') continue;
+
+            dist[nx][ny] = dist[cur.x][cur.y]+1;
+            Q.push({nx, ny});
+        }
+    }
+
+    cout << dist[n-1][m-1]+1;
+}
