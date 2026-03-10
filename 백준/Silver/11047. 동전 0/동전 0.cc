5da4d76ffc3a44c756bb0e01f15f6a77fb33acbc
choiJieun <eaee20171@gmail.com>
2026-03-10 11:08:10 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -2,7 +2,7 @@
 using namespace std;
 
 int coin[12];
-int n,m,t;
+int n,m;
 
 int main() {
     ios::sync_with_stdio(false);
@@ -10,20 +10,10 @@ int main() {
 
     int cnt=0;
     cin >> n >> m;
-    for (int i=0; i<n; ++i) {
-        cin >> t;
-        if (t <= m) coin[i] = t;
-    }
-    sort(coin, coin+n, greater<int>());
-
-    for (int i=0; i<n; ++i) {
-        if (coin[i] <= m) {
-            t = m / coin[i];
-            m -= (m / coin[i] *coin[i]);
-            cnt += t;
-        }
-
-        if (m == 0) break;
+    for (int i=0; i<n; ++i) cin >> coin[i];
+    for (int i=n-1; i>=0; --i) {
+        cnt += m / coin[i];
+        m = m % coin[i];
     }
 
     cout << cnt;
